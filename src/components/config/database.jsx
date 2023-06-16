49d622e5874fd4543559560754fb259970307605
Luis Florez <braian1k3@gmail.com>
2023-06-16 14:11:01 -0500

    Añadir las configuraciones del firestore, por ende importar uuid y afectar los package.json.

---
+++ src/components/config/database.jsx
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDr5_UMe5KAyVBOOGmF3y2AODgLBDnG3s0",
+  authDomain: "project-variety.firebaseapp.com",
+  projectId: "project-variety",
+  storageBucket: "project-variety.appspot.com",
+  messagingSenderId: "394035243254",
+  appId: "1:394035243254:web:89cc849b04e4c86131a4d7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const dataBase = getFirestore(app);
+export const storage = getStorage(app);
+
+export const saveFile = async (file) => {
+    const uploadFile = ref(storage, v4());
+    await uploadBytes(uploadFile, file);
+    const urlFile = await getDownloadURL(uploadFile);
+    return urlFile;
+};
