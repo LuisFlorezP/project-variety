299944f5bb4d3b20413da45439f7461c1072a5a8
Luis Florez <braian1k3@gmail.com>
2023-06-25 22:14:18 -0500

    Crear componentes para trabajar los formularios para los procesos del admin.

---
+++ src/pages/Admin/AdminForms/FormsProductsAdmin.jsx
@@ -0,0 +1,9 @@
+const FormsProductsAdmin = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default FormsProductsAdmin;
--- src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
+++ src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const CustomersAdmin = () => {
     return (
         <>
-        
+            <section>
+                <button>Volver</button>
+                <button>Crear</button>
+            </section>
+            <section>
+                <Link to={""} />
+            </section>
         </>
     );
 };
