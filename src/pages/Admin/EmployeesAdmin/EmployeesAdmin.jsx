d585818344194eae9aa68864ab21b284cc17038a
Luis Florez <braian1k3@gmail.com>
2023-06-26 11:08:50 -0500

    Trabajar los archivos admin para mostrar los registros existentes en firebase, con el fin de continuar al trabajo de editar y eliminar cada registro.

--- src/pages/Admin/EmployeesAdmin/EmployeesAdmin.jsx
+++ src/pages/Admin/EmployeesAdmin/EmployeesAdmin.jsx
@@ -1,15 +1,45 @@
+import { Link } from "react-router-dom";
+import { dataBase } from "../../../components/config/database.jsx";
+import { collection, getDocs } from "@firebase/firestore";
+import { useEffect, useState } from "react";
 import NavbarAdmin from "../../../components/NavbarAdmin/NavbarAdmin";
 
 const EmployeesAdmin = () => {
+
+    const [employees, setEmployees] = useState([]);
+
+    const readEmployees = async () => {
+        const data = await getDocs(collection(dataBase, "empleado"));
+        setEmployees(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+
+    useEffect(() => {
+        readEmployees();
+    }, []);
+
     return (
-        <>
-            <NavbarAdmin
-                form={"/formemployeesadmin"}
-            />
+        <section className="contenedor">
+            <NavbarAdmin form={"/formemployeesadmin"} />
             <section>
-                
+                {
+                    employees.map((employee) => (
+                        <section key={employee.id}>
+                            <h1>{employee.nombre}</h1>
+                            <p>{employee.cargo}</p>
+                            <p>{employee.correo}</p>
+                            <p>{employee.direccion_residencia}</p>
+                            <p>{employee.documento}</p>
+                            <p>{employee.numero_cuenta_bancaria}</p>
+                            <p>{employee.salario}</p>
+                            <img src={employee.imagen} alt="" />
+                            <Link to={""}>Editar</Link>
+                            <input type="button" value={'Eliminar'} />
+                            <hr />
+                        </section>
+                    ))
+                }
             </section>
-        </>
+        </section>
     );
 };
 
