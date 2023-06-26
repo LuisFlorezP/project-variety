a7ed3f12b5d8d20a53661c235a50f881be9e2734
DanielCaroGutierrez <69063214+DanielCaroGutierrez@users.noreply.github.com>
2023-06-25 22:16:38 -0500

    Se hace la creacion de los cards admin y se emplea en adminDasboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AdminLogin from "../components/AdminLogin/AdminLogin";
+import CardAdmin from "../components/AdminCard/CardAdmin";
 
 const AdminDashboard = () => {
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
@@ -7,7 +8,7 @@ const AdminDashboard = () => {
   const handleLogin = (username, password) => {
 
     // Verificación de credenciales para mostrar elementos 
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "123") { 
       setIsAdminLoggedIn(true);
     } else {
       setIsAdminLoggedIn(false);
@@ -17,7 +18,18 @@ const AdminDashboard = () => {
   return (
     <>
       {isAdminLoggedIn ? (
-        <h1>Admin Dashboard</h1>
+            <div>
+            <CardAdmin
+              title={"Customers"}
+              description={"Administrar pagina de los customers"}
+              link={""}
+            />
+              <CardAdmin
+              title={"product"}
+              description={"Administrar pagina de los products"}
+              link={""}
+            />
+          </div>
       ) : (
         <AdminLogin handleLogin={handleLogin} />
       )}
