e627fc3623a24614876d38de420c874e2a9e6052
Luis Florez <braian1k3@gmail.com>
2023-06-25 23:51:44 -0500

    Comenzar opciones de traer customers.

--- src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
+++ src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
@@ -1,6 +1,25 @@
 import { Link } from "react-router-dom";
+import { database } from "../../../components/config/database.jsx";
+import { collection, getDocs } from "@firebase/firestore";
+import { useEffect, useState } from "react";
 
 const CustomersAdmin = () => {
+
+    const [customers, setCustomers] = useState([]);
+
+    const readCustomers = async () => {
+        const customersDatabase = collection(database, "cliente");
+
+        const data = await getDocs(customersDatabase);
+
+        setCustomers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+
+    };
+
+    useEffect(() => {
+        readCustomers()
+    }, [])
+
     return (
         <>
             <section>
@@ -8,8 +27,24 @@ const CustomersAdmin = () => {
                 <Link to={"/formcustomersadmin"}>Crear</ Link>
             </section>
             <section>
-                
-            </section>
+                {
+                    customers.map((customer) => (
+                        <section key={customer.id}>
+                            <h1>{customer.nombre}</h1>
+                            <p>{customer.barrio}</p>
+                            <p>{customer.ciudad}</p>
+                            <p>{customer.correo}</p>
+                            <p>{customer.direccion}</p>
+                            <p>{customer.documento}</p>
+                            <p>{customer.telefono}</p>
+                            <p>{customer.valor}</p>
+                            <Link to={"/"}>Editar</Link>
+                            <input type="button" value={'Eliminar'} />
+                            <hr />
+                        </section>
+                    ))
+                }
+        </section>
         </>
     );
 };
