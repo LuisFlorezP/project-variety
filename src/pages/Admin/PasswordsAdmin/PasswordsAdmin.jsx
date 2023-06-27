776d513b912de595fd7695e849a7a2474b052b32
QuarcissusDev <juacaramirezlo@gmail.com>
2023-06-27 11:39:23 -0500

    Funciones de eliminar en admin con formateo

--- src/pages/Admin/PasswordsAdmin/PasswordsAdmin.jsx
+++ src/pages/Admin/PasswordsAdmin/PasswordsAdmin.jsx
@@ -6,56 +6,60 @@ import { useEffect, useState } from "react";
 import NavbarAdmin from "../../../components/NavbarAdmin/NavbarAdmin";
 
 const PasswordsAdmin = () => {
+  const [passwords, setPasswords] = useState([]);
 
-    const [passwords, setPasswords] = useState([]);
+  const readPasswords = async () => {
+    const data = await getDocs(collection(dataBase, "password"));
+    setPasswords(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
 
-    const readPasswords = async () => {
-        const data = await getDocs(collection(dataBase, "password"));
-        setPasswords(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-    };
+  const deletePassword = async (id) => {
+    const deletedPassword = doc(dataBase, "password", id);
+    await deleteDoc(deletedPassword);
+    setPasswords(passwords.filter((password) => password.id !== id));
+  };
 
-    const deletePassword = async (id) => {
-        const deletedPassword = doc(dataBase,"password",id)
-        await deleteDoc(deletedPassword);
-        setPasswords(passwords.filter((password) => password.id !== id));
-    }
+  useEffect(() => {
+    readPasswords();
+  }, []);
 
-
-
-    useEffect(() => {
-        readPasswords();
-    }, []);
-
-    return (
-        <>
-        <NavbarAdmin form={"/formpasswordsadmin"} />
-            <section className="card-passwords">
-            {
-                passwords.map((password) => (
-                    <section key={password.id}>
-                        <section className="card-header-passwords">
-                            <h1>{password.usuario}</h1>
-                        </section>
-                        <section className="card-body-passwords">
-                            <section className="card-image-passwords">
-                                <img src={password.imagen_aplicacion} alt="" />
-                                <img src={password.imagen} alt="" />
-                            </section>
-                            <section className="card-info-passwords">
-                                <p>{password.password_usuario}</p>
-                                <p>{password.nombre_aplicacion}</p>
-                            </section>
-                        </section>
-                        <section className="buttons-passwords">
-                            <Link to={""} className='editar-passwords'>Editar</Link>
-                            <input type="button" className='eliminar-passwords' value={'Eliminar'} onClick={()=>{deletePassword(password.id)}} />
-                        </section>
-                    </section>
-                ))
-            }
-         </section>
-        </>
-        );
+  return (
+    <>
+      <NavbarAdmin form={"/formpasswordsadmin"} />
+      <section className="card-passwords">
+        {passwords.map((password) => (
+          <section key={password.id}>
+            <section className="card-header-passwords">
+              <h1>{password.usuario}</h1>
+            </section>
+            <section className="card-body-passwords">
+              <section className="card-image-passwords">
+                <img src={password.imagen_aplicacion} alt="" />
+                <img src={password.imagen} alt="" />
+              </section>
+              <section className="card-info-passwords">
+                <p>{password.password_usuario}</p>
+                <p>{password.nombre_aplicacion}</p>
+              </section>
+            </section>
+            <section className="buttons-passwords">
+              <Link to={""} className="editar-passwords">
+                Editar
+              </Link>
+              <input
+                type="button"
+                className="eliminar-passwords"
+                value={"Eliminar"}
+                onClick={() => {
+                  deletePassword(password.id);
+                }}
+              />
+            </section>
+          </section>
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default PasswordsAdmin;
