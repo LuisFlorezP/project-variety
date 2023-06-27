86fbfc8f190b0cf0b7d493c21b70d59f3d77b079
QuarcissusDev <juacaramirezlo@gmail.com>
2023-06-26 21:30:29 -0500

    generando función de borrar en customers - admin

--- src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
+++ src/pages/Admin/CustomersAdmin/CustomersAdmin.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { dataBase } from "../../../components/config/database.jsx";
-import { collection, getDocs } from "@firebase/firestore";
+import { collection, getDocs, doc,deleteDoc } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import NavbarAdmin from "../../../components/NavbarAdmin/NavbarAdmin";
 
@@ -12,6 +12,25 @@ const CustomersAdmin = () => {
         const data = await getDocs(collection(dataBase, "cliente"));
         setCustomers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     };
+
+    const deleteCustomer = async (id) =>  {
+        const deletedCustomer = doc(dataBase,"cliente",id)
+        await deleteDoc(deletedCustomer);
+        readCustomers();
+    }
+
+
+
+
+
+
+    /* const eliminarServicio = async(id) => {
+    const servicioEliminado = doc(dataBase,"servicios",id) //Me recibe la base de datos, el nombre y el id. (Por parametro)
+    await deleteDoc(servicioEliminado)
+    mostrarServicios()
+
+  }
+ */
 
     useEffect(() => {
         readCustomers();
@@ -34,7 +53,7 @@ const CustomersAdmin = () => {
                             <p>{customer.valor}</p>
                             <img src={customer.imagen} alt="" />
                             <Link to={""}>Editar</Link>
-                            <input type="button" value={'Eliminar'} />
+                            <input type="button" value={'Eliminar'} onClick={()=> {deleteCustomer(customer.id)}} />
                             <hr />
                         </section>
                     ))
