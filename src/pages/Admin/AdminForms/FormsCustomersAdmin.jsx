b3d959b4decd80ed0a3fbdee16a5c637ce62cfd5
DanielCaroGutierrez <69063214+DanielCaroGutierrez@users.noreply.github.com>
2023-06-29 10:34:10 -0500

    Empezar a trabajar estilos en los formularios.

--- src/pages/Admin/AdminForms/FormsCustomersAdmin.jsx
+++ src/pages/Admin/AdminForms/FormsCustomersAdmin.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { collection, addDoc } from "@firebase/firestore";
 import { dataBase, saveFile } from "../../../components/config/database";
+import "../FormsGeneral.css";
+
 
 const FormsCustomersAdmin = () => {
     const [nombre, setNombre] = useState('');
@@ -33,31 +35,34 @@ const FormsCustomersAdmin = () => {
     };
 
     return (
-        <>
-            <NavbarAdminForm comeback={"/customersadmin"} />
-            <section>
-                <form action="">
-                    <label>Name:</label>
-                    <input type={"text"} id={"nombre"} placeholder={"Enter customer name..."} onChange={e => setNombre(e.target.value)} /><br />
-                    <label>Document:</label>
-                    <input type={"text"} id={"documento"} placeholder={"Enter identity document number..."} onChange={e => setDocumento(e.target.value)} /><br />
-                    <label>Phone:</label>
-                    <input type={"text"} id={"telefono"} placeholder={"Enter phone number..."} onChange={e => setTelefono(e.target.value)} /><br />
-                    <label>Email:</label>
-                    <input type={"text"} id={"correo"} placeholder={"Enter email..."} onChange={e => setCorreo(e.target.value)} /><br />
-                    <label>Address:</label>
-                    <input type={"text"} id={"correo"} placeholder={"Enter the address of residence..."} onChange={e => setDireccion(e.target.value)} /><br />
-                    <label>City:</label>
-                    <input type={"text"} id={"ciudad"} placeholder={"Enter the city of residence..."} onChange={e => setCiudad(e.target.value)} /><br />
-                    <label>Neighborhood:</label>
-                    <input type={"text"} id={"barrio"} placeholder={"Enter the neighborhood of residence..."} onChange={e => setBarrio(e.target.value)} /><br />
-                    <label>Customer image:</label>
-                    <input type={"file"} id={"imagen"} onChange={e => setFile(e.target.files[0])} /><br />
-                    <input type={"button"} value={"Submit"} onClick={saveCustomer} />
-                </form>
-            </section>
-        </>
-    );
+<>
+  <section className="navbar-container">
+    <NavbarAdminForm comeback={"/customersadmin"} />
+  </section>
+  <section className="section-container">
+    <form className="form-container" action="">
+      <label className="form-label">Name:</label>
+      <input className="form-input" type="text" id="nombre" placeholder="Enter customer name..." onChange={e => setNombre(e.target.value)} /><br />
+      <label className="form-label">Document:</label>
+      <input className="form-input" type="text" id="documento" placeholder="Enter identity document number..." onChange={e => setDocumento(e.target.value)} /><br />
+      <label className="form-label">Phone:</label>
+      <input className="form-input" type="text" id="telefono" placeholder="Enter phone number..." onChange={e => setTelefono(e.target.value)} /><br />
+      <label className="form-label">Email:</label>
+      <input className="form-input" type="text" id="correo" placeholder="Enter email..." onChange={e => setCorreo(e.target.value)} /><br />
+      <label className="form-label">Address:</label>
+      <input className="form-input" type="text" id="correo" placeholder="Enter the address of residence..." onChange={e => setDireccion(e.target.value)} /><br />
+      <label className="form-label">City:</label>
+      <input className="form-input" type="text" id="ciudad" placeholder="Enter the city of residence..." onChange={e => setCiudad(e.target.value)} /><br />
+      <label className="form-label">Neighborhood:</label>
+      <input className="form-input" type="text" id="barrio" placeholder="Enter the neighborhood of residence..." onChange={e => setBarrio(e.target.value)} /><br />
+      <label className="form-label">Customer image:</label>
+      <input className="form-input" type="file" id="imagen" onChange={e => setFile(e.target.files[0])} /><br />
+      <input className="form-submit" type="button" value="Submit" onClick={saveCustomer} />
+    </form>
+  </section>
+</>
+);
+
 };
 
 export default FormsCustomersAdmin;
